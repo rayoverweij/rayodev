2853d3b88759d411bcc4791ec90161b914e71203
Rayo Verweij <rayo.verweij@live.nl>
2025-06-09 14:11:56 +0100

    Design updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,12 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
