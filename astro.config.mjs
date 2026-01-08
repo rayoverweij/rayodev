28133b8d2aa4a4ebb71ffc3f9d24dd4e5e1ce14f
Rayo Verweij <rayo.verweij@live.nl>
2026-01-08 22:54:37 +0000

    Refinements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import tunnel from "astro-tunnel";
 
 
 export default defineConfig({
-	adapter: vercel(),
 	integrations: [tunnel()],
 	vite: {
 		plugins: [tailwindcss()]
+	},
+	image: {
+		layout: "constrained"
 	}
 });
